c44e7f8dfaf0c76377cb2d3b7dc1a48f5691f5f3
HeroChan <andrew@swiftevolutions.com>
2017-12-03 17:50:13 -0500

    Added layers and click to move

---
+++ Ahoy Matey RTS/Ahoy Matey RTS/Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour{
+
+    private Vector3 inputValue;
+    private Material PlayerMaterial;
+
+	// Use this for initialization
+	void Start () {
+        PlayerMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer)
+        {
+            return;
+            
+        }
+        else
+        {
+           
+            PlayerMaterial.color = Color.blue;
+            inputValue.x = CrossPlatformInputManager.GetAxis("Vertical");
+            inputValue.y = 0f;
+            inputValue.z = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            transform.Translate(inputValue);
+        }
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+}
